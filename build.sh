422e26991ec027bec127373a7e973cc56a93922d
Sanjay <sanjayskpy1@gmail.com>
2026-05-07 01:25:46 +0530

    Add superuser credentials to .env and update build script for setup

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py runserver
+python manage.py createsuperuser --noinput
