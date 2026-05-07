0af99ae8b77641efcce6b828d89260879dfd21e6
Sanjay <sanjayskpy1@gmail.com>
2026-05-07 16:48:52 +0530

    Refactor build script to improve pip installation commands and ensure error handling

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -o errexit
 
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip
+
+# Clean install (VERY IMPORTANT)
+pip install --no-cache-dir -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
