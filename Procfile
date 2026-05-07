0925d38aaa82f9226e88ddbaf4e1e377a5b286a7
Sanjay <sanjayskpy1@gmail.com>
2026-05-07 13:35:26 +0530

    Remove Django installation from build script and clean up Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn core.wsgi:application --bind 0.0.0.0:$PORT --log-file -
+web: gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -e
 
 python -m pip install --upgrade pip
-pip install Django
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
