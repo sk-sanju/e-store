53b3f80e018b87e2cb991b7554ce69774924272a
Sanjay <sanjayskpy1@gmail.com>
2026-05-07 13:54:10 +0530

    Refactor ALLOWED_HOSTS to use a static list instead of environment variable

--- core/settings.py
+++ core/settings.py
@@ -18,10 +18,11 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 # Render + production safe hosts
-ALLOWED_HOSTS = env.list(
-    'ALLOWED_HOSTS',
-    default=['127.0.0.1', 'localhost', 'e-store-gjav.onrender.com']
-)
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "e-store-gjav.onrender.com",
+]
 
 # Application definition
 INSTALLED_APPS = [
